d59fa491b55a393a87fe5304f1f766722afe1361
125K <bluearduino20@gmail.com>
2020-05-01 17:45:13 +0200

    Update fix-fivem.cmd

--- fix-fivem.cmd
+++ fix-fivem.cmd
@@ -2,7 +2,9 @@ REM Elimina la memoria caché que provoca el error:
 REM "Failed to getinfo server after 3 attempts"
 
 @echo off
-rmdir /s /q %appdata%\..\Local\FiveM\FiveM.app\cache\priv
+cd %appdata%\..\Local\FiveM\FiveM.app\cache
+
+rmdir /s /q priv, db
 
 echo Proceso finalizado exitosamente
 pause > nul
